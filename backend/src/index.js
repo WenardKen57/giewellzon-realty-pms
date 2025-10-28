c1f6a2a15de643b67d90dea4e4628f10bf4d860d
Desha <deshadespa@gmail.com>
2025-10-28 18:47:49 +0800

    for local development change cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,6 +22,7 @@ console.log(
 const app = express();
 
 app.use(helmet());
+/* FOR DEPLOYMENT
 app.use(
   cors({
     origin: [
@@ -32,6 +33,8 @@ app.use(
     credentials: true,
   })
 );
+*/
+app.use(cors());
 
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true }));
